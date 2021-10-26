56afa7898b372af40930244eeabc70c9346759c7
João Corrêa <jfrcorrea@gmail.com>
2021-10-26 18:25:33 +0000

    tests on batches and allocations

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ Classes that represent entities of the domain
 
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Iterable, Optional, Set
 
 
 @dataclass(frozen=True)
@@ -21,12 +21,57 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Batch):
+            return NotImplemented
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other: object) -> bool:
+        if not isinstance(other, Batch):
+            return NotImplemented
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, Batch):
+            return NotImplemented
+        return not self.__gt__(other)
 
     def allocate(self, line: OrderLine) -> None:
         """Allocate a order line to the batch"""
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        """Deallocate a line from the batch"""
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     def can_allocate(self, line: OrderLine) -> bool:
         """Check if can allocate a Order Line to the Batch"""
         return self.sku == line.sku and self.available_quantity >= line.qty
+
+    @property
+    def allocated_quantity(self) -> int:
+        """Total allocated quantity"""
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        """Available quantity in the batch """
+        return self._purchased_quantity - self.allocated_quantity
+
+def allocate(line: OrderLine, batches: Iterable[Batch]) -> str:
+    """Alocate a line in one of a set of batches"""
+    batch = [b for b in sorted(batches) if b.can_allocate(line)][0]
+    batch.allocate(line)
+    return batch.reference
